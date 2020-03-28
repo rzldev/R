be4c3ec76757259eb153afcf2d6b2edd1f86f7bc
rzldev <49812100+rzldev@users.noreply.github.com>
2020-03-28 10:07:32 +0700

    R Loops Statement

---
+++ S2-Fundamentals/F2-Loops/ForLoops.R
@@ -0,0 +1,11 @@
+## Loops are used in programming to repeat a specific block of code
+
+for (i in 1:5) {
+  print(i)
+  print('Hello')
+}
+
+for (i in 5:10) {
+  print(i)
+  print('Hello')
+}
---
+++ S2-Fundamentals/F2-Loops/WhileLoops.R
@@ -0,0 +1,12 @@
+## Loops are used in programming to repeat a specific block of code
+
+while (TRUE) {
+  print('Hello')
+}
+
+## Create a loop with specific condition
+counter <- 1
+while (counter <= 10) {
+  print(counter)
+  counter <- counter + 1
+}
