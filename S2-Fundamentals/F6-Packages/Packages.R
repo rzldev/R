4e1f6a4cb4853099d2f0e63c1e5ec26f694798ab
rzldev <49812100+rzldev@users.noreply.github.com>
2020-03-29 07:47:07 +0700

    R Packages

---
+++ S2-Fundamentals/F6-Packages/Packages.R
@@ -0,0 +1,12 @@
+## In R, the fundamental unit of shareable code is the package. 
+# A package bundles together code, data, documentation, and tests, and is easy to share with others.
+
+## How to install a Package
+install.packages('ggplot2')
+
+## Import the package
+library(ggplot2)
+
+## Know more about the package
+?ggplot2()
+
